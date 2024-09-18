30027811a68f1271c6451a8c131f1339d24032de
กัมแพงเพชร สิงห์ขรณ์ <kampang.ks89@gmail.com>
2024-09-18 17:57:47 +0700

    Initial commit

---
+++ src/main/java/com/housely/houselywebsite/HouselyWebSiteApplication.java
@@ -0,0 +1,13 @@
+package com.housely.houselywebsite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HouselyWebSiteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HouselyWebSiteApplication.class, args);
+    }
+
+}

b276364dd69db586242d59ec0e90d94f7a0e4718
Taksanant <taksamerr@gmail.com>
2024-09-20 00:49:41 +0700

    up

---
+++ src/main/java/com/housely/model/CartItem.java
@@ -0,0 +1,17 @@
+package com.housely.model;
+
+
+public class CartItem {
+   
+    private CartItemKey id;
+    private Product product;
+    private Cart cart;
+    private Long quantity;
+
+    
+
+
+
+
+
+}
---
+++ src/main/java/com/housely/model/CartItemKey.java
@@ -0,0 +1,5 @@
+package com.housely.model;
+
+public class CartItemKey {
+
+}

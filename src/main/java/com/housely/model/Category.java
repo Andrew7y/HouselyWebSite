0fe461af364c461224bc3622c51f145a5b1bd5b7
Taksanant <taksamerr@gmail.com>
2024-09-20 01:22:34 +0700

    up

---
+++ src/main/java/com/housely/model/Category.java
@@ -0,0 +1,24 @@
+package com.housely.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.List;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Category {
+
+
+    private Long categoryId;
+    private String categoryName;
+    private String description;
+    private String imageBase64;
+    private List<Product> productInCategories;
+
+}
+

8865c00d2ca134bd124f47372a8ba3ea581d2ca7
Muttita <muttita.c@kkumail.com>
2024-09-20 21:48:04 +0700

    up

--- src/main/java/com/housely/houselywebsite/AdminConfig.java
+++ src/main/java/com/housely/houselywebsite/AdminConfig.java
@@ -2,13 +2,12 @@ package com.housely.houselywebsite;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
-import org.springframework.web.reactive.function.client.WebClient;
 import org.springframework.context.annotation.Bean;
 
 @Configuration
 public class AdminConfig {
 
-     @Bean
+    @Bean
     public RestTemplate restTemplate() {
         return new RestTemplate();
     }

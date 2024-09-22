b0922bce0f46b0f31f113bc958508cb10b1bfd9a
punho1 <tidapornterm2018@gmail.com>
2024-09-23 01:00:09 +0700

    Update

--- src/main/java/com/housely/houselywebsite/service/ShippingAddressService.java
+++ src/main/java/com/housely/houselywebsite/service/ShippingAddressService.java
@@ -1,5 +1,20 @@
 package com.housely.houselywebsite.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.client.WebClient;
+
+@Service
 public class ShippingAddressService {
 
+    private final WebClient webClient;
+    private final OrderItemService orderItemService;
+
+    @Autowired
+    public ShippingAddressService(WebClient webClient, OrderItemService orderItemService){
+        this.webClient = webClient;
+        this.orderItemService = orderItemService;
+    }
+
+    public 
 }
